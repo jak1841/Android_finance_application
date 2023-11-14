e59587edd5c6c76711822accc5d88c16cfeca1f8
jak1841 <jaskarnd16@gmail.com>
2023-11-14 14:27:35 -0600

    First commit

---
+++ app/src/main/java/com/example/financeapplication/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.financeapplication
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Your initialization and setup code goes here
+    }
+
+
+}
