8b9c3f1368223f88a05c523b5fefc7fa8b133961
jak1841 <jaskarnd16@gmail.com>
2023-11-14 18:47:28 -0600

    Created 3 different template screens

---
+++ app/src/main/java/com/example/financeapplication/InsightsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.financeapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InsightsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insights)
+
+
+    }
+}
